dc2b5fbfae9c3c2b955eee8ef5566d920f2fa375
Torcsvari Gergo <torcsvari.gergo@wanari.com>
2017-08-20 14:14:53 +0200

    chartService functions implemented w/o tests

--- src/main/scala/com/github/tg44/claymore/service/ChartService.scala
+++ src/main/scala/com/github/tg44/claymore/service/ChartService.scala
@@ -1,6 +1,6 @@
 package com.github.tg44.claymore.service
 
-import com.github.tg44.claymore.repository.measures.{Measure, MeasureRepo, StatisticData}
+import com.github.tg44.claymore.repository.measures.{CurrencyInformation, Measure, MeasureRepo, StatisticData}
 import com.github.tg44.claymore.utils.GeneralUtil
 import scaldi.{Injectable, Injector}
 
@@ -16,14 +16,31 @@ class ChartService(implicit injector: Injector, ec: ExecutionContextExecutor) ex
 
   private def computeChartsData(measures: Seq[Measure]) = {
     val aggregatedMeasuresByHost = aggregateMeasuresByHost(measures)
+    val aggregatedCurrenciesByTime = aggregateCurrenciesByTime(measures)
     Charts(
       computeAvgTempPerHost(aggregatedMeasuresByHost),
       computeRuntimePerHost(aggregatedMeasuresByHost),
-      Seq(),
-      Seq(),
+      computeHashratePerCurrencie(aggregatedCurrenciesByTime),
+      computeSharesPerCurrencie(aggregatedCurrenciesByTime),
       computeTempPerHostPerCard(aggregatedMeasuresByHost),
       computeFanPerHostPerCard(aggregatedMeasuresByHost)
     )
+  }
+
+  private def computeHashratePerCurrencie(aggregatedCurrenciesByTime: Map[String, Map[Long, Seq[CurrencyInformation]]]) = {
+    val data = aggregatedCurrenciesByTime.mapValues(_.map(createHashRateChartData).toList)
+
+    mapToSingleLineChartSeq(data)
+  }
+
+  private def computeSharesPerCurrencie(aggregatedCurrenciesByTime: Map[String, Map[Long, Seq[CurrencyInformation]]]): Seq[MultilineChart] = {
+    val data: Map[String, (Iterable[ChartData], Iterable[ChartData], Iterable[ChartData])] =
+      aggregatedCurrenciesByTime.mapValues(_.map(createSharesChartData).unzip3)
+
+    data.map {
+      case (key, value) =>
+        MultilineChart(Seq(value._1.toList, value._2.toList, value._3.toList), Seq("valid", "invalid", "rejected"), key)
+    }.toList
   }
 
   private def computeAvgTempPerHost(aggregatedMeasuresByHost: Map[String, Seq[StatisticData]]): MultilineChart = {
@@ -71,6 +88,34 @@ class ChartService(implicit injector: Injector, ec: ExecutionContextExecutor) ex
       .mapValues(dataList => dataList.sortBy(data => data.timeStamp))
   }
 
+  private def aggregateCurrenciesByTime(measures: Seq[Measure]): Map[String, Map[Long, Seq[CurrencyInformation]]] = {
+    val lower = measures.minBy(_.fromTimeStamp).fromTimeStamp
+    val measureToMax = measures.maxBy(_.toTimeStamp).toTimeStamp
+    val upper = if (measureToMax > GeneralUtil.nowInUnix) GeneralUtil.nowInUnix else measureToMax
+
+    val timestamps = GeneralUtil.generateTimeStamps(lower, upper, 300)
+
+    measures
+      .flatMap(_.data)
+      .flatMap(statData => statData.currencyInformations.map(currInfo => (currInfo.currency, statData.timeStamp, currInfo)))
+      .foldLeft(Map[String, Seq[(Long, CurrencyInformation)]]()) {
+        case (acc, (currency, ts, info)) =>
+          acc + (currency -> (acc.getOrElse(currency, Seq.empty[(Long, CurrencyInformation)]) ++ Seq((ts, info))))
+      }
+      .map {
+        case (currency, timeMeasureList) =>
+          currency -> aggregateByTime(timeMeasureList, timestamps)
+      }
+  }
+
+  private def aggregateByTime(rawData: Seq[(Long, CurrencyInformation)], timeStamps: Seq[Long]): Map[Long, Seq[CurrencyInformation]] = {
+    rawData.foldLeft(Map[Long, Seq[CurrencyInformation]]()) {
+      case (acc, element) =>
+        val convertedTs: Long = timeStamps.foldLeft(timeStamps.head)((a, c) => if (c < element._1) c else a)
+        acc + (convertedTs -> (acc.getOrElse(convertedTs, Seq.empty[CurrencyInformation]) ++ Seq(element._2)))
+    }
+  }
+
   private def mapToSingleLineChartSeq(map: Map[String, Seq[ChartData]]) = {
     map.map { case (title, data) => SingleLineChart(data, title) }.toList
   }
@@ -90,12 +135,24 @@ class ChartService(implicit injector: Injector, ec: ExecutionContextExecutor) ex
   private def createFanChartData(data: StatisticData): Seq[ChartData] = {
     data.cards.map(stat => ChartData(GeneralUtil.convertTimeStampToChartString(data.timeStamp), stat.fan))
   }
+
+  private def createHashRateChartData(rawData: (Long, Seq[CurrencyInformation])): ChartData = {
+    ChartData(GeneralUtil.convertTimeStampToChartString(rawData._1), rawData._2.map(_.sumHR).sum)
+  }
+
+  private def createSharesChartData(rawData: (Long, Seq[CurrencyInformation])): (ChartData, ChartData, ChartData) = {
+    (
+      ChartData(GeneralUtil.convertTimeStampToChartString(rawData._1), rawData._2.map(_.shares).sum),
+      ChartData(GeneralUtil.convertTimeStampToChartString(rawData._1), rawData._2.map(_.invalidShares).sum),
+      ChartData(GeneralUtil.convertTimeStampToChartString(rawData._1), rawData._2.map(_.sharesRejected).sum)
+    )
+  }
 }
 
 case class Charts(
     avgTempPerHost: MultilineChart,
     runtimePerHost: Seq[SingleLineChart], //in days
-    currencyHashrateCharts: Seq[MultilineChart],
+    currencyHashrateCharts: Seq[SingleLineChart],
     sharesPerCurrencies: Seq[MultilineChart],
     tempPerCardPerHost: Seq[MultilineChart],
     fanPerCardPerHost: Seq[MultilineChart]
--- src/main/scala/com/github/tg44/claymore/utils/GeneralUtil.scala
+++ src/main/scala/com/github/tg44/claymore/utils/GeneralUtil.scala
@@ -9,11 +9,17 @@ object GeneralUtil {
   // if one statdata is ~2kb, 1 user has 50 rig and send 1 measure/min
   // the data in one mongo doc will be < 2kb*50*180 = 17,57Mb  >>16Mb mongo limit
   // with this period number 50 rig and 1 measure/min is a hard limit!!!
-  def generatePeriod(time: Long): (Long, Long) = {
+  def generatePeriod(time: Long, frame: Long = 10000): (Long, Long) = {
     (
-      (time - 1) / 10000 * 10000 + 1,
-      (((time - 1) / 10000) + 1) * 10000
+      (time - 1) / frame * frame + 1,
+      (((time - 1) / frame) + 1) * frame
     )
+  }
+
+  def generateTimeStamps(lowerBound: Long, upperBound: Long, frame: Long): Seq[Long] = {
+    val lower = (lowerBound - 1) / frame * frame
+    val upper = (((upperBound - 1) / frame) + 1) * frame
+    lower to upper by frame
   }
 
   def convertTimeStampToChartString(ts: Long) = {
